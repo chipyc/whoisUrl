87d1b56bc318c538bd512633ecdcc122c3165e95
chipyc <chipeculya@gmail.com>
2019-03-26 12:10:02 +0300

    Comments

--- whs.py
+++ whs.py
@@ -2,13 +2,14 @@ import sys
 from urllib.parse import urlsplit
 
 
-
 def domains():
+    #getting string with URL
     domain = str(sys.argv[1])
+    #splitting url with urllib
     splited = urlsplit(domain)
+    #getting xxx.yyy part from url
     if splited.netloc != '':
         domain = (splited.netloc)
+    #converting to idn and deleting "b''" part from it
     domain = domain.encode("idna").decode("utf-8")
     print(domain)
-
-
